cf261d8b9b44901fd45be36a0121ad8e281413ca
Sam Ritchie <samritchie@google.com>
2020-06-30 08:58:19 -0600

    update

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject maturin "0.1.0-SNAPSHOT"
+  :description "Lagrange meets the space turtle!"
+  :url "https://github.com/sritchie/maturin"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [net.littleredcomputer/sicmutils "0.12.1"]
+                 [quil "3.1.0"]])
