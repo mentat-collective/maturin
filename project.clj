ea4d10b2aa9e396caa3920eaec067f8e01200515
Adam Haber <adamhaber@gmail.com>
2020-11-26 22:19:09 +0200

    Added dist deps

--- project.clj
+++ project.clj
@@ -5,4 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [net.littleredcomputer/sicmutils "0.13.1-SNAPSHOT"]
-                 [quil "3.1.0"]])
+                 [quil "3.1.0"]
+                 [distributions "0.1.2"]
+                 [incanter/incanter-charts "1.9.3"]])
