999b1598b049d1418b5398502fa8f7c4a52d185d
Adam Haber <adamhaber@gmail.com>
2020-12-10 08:59:13 +0200

    Added L-springs and L-regression-compsed

--- src/maturin/springs.clj
+++ src/maturin/springs.clj
@@ -1,58 +1,79 @@
+(require 'maturin.better)
+(in-ns 'maturin.better)
+
 ;; create xs
 (defn gen-xs [n]
-    (take n (repeatedly #(rand 10))))
-  
+  (take n (repeatedly #(rand 10))))
+
 ;; create linear regression mock data
 (defn gen-ys [xs slope intercept]
-    (map #(+ (d/sample (d/normal 0 1)) intercept (* % slope)) xs))
+  (map #(+ (d/sample (d/normal 0 1)) intercept (* % slope)) xs))
 
 ;; return linear function
 (defn linear-func [slope intercept]
-    (fn [x] (+ intercept (* slope x)))) 
+  (fn [x] (+ intercept (* slope x))))
+
+(defn line->offsets [xs ys]
+  (fn [[slope intercept]]
+    (let [preds (map (linear-func slope intercept) xs)]
+      (mapv - preds ys))))
+
+(defn L-springs [k]
+  (fn [diffs]
+    (- (* 1/2 k (square diffs)))))
+
+(defn L-regression-composed [xs ys k]
+  (compose (L-springs k) (F->C (line->offsets xs ys))))
 
 ;; lagrangian for the springs-rod system, regression case:
 ;; springs constrained to have the same x-coordiate on both axes = minimize MSE
 (defn L-regression
-    [xs ys k]
-    (fn [[_ [slope intercept] _]]
-        (let [preds (map (linear-func slope intercept) xs)
-            diffs (mapv - preds ys)]
-            (- (* 1/2 k (square diffs))))))
+  [xs ys k]
+  (fn [[_ [slope intercept] _]]
+    (let [preds (map (linear-func slope intercept) xs)
+          diffs (mapv - preds ys)]
+      (- (* 1/2 k (square diffs))))))
 
 (defn distance-point-line [slope intercept]
-    (fn [x y]
-        (/ (Math/abs (+ (* slope x) (- y) intercept)) (Math/sqrt (square [slope 1])))))
+  (fn [x y]
+    (/ (Math/abs (+ (* slope x) (- y) intercept)) (Math/sqrt (square [slope 1])))))
 
 ;; lagrangian for the springs-rod system, PCA case:
 ;; springs can "slide" on the bar's end = minimize point-line distance
 (defn L-PCA
-    [xs ys k]
-    (fn [[_ [slope intercept] _]]
-        (let [diffs (mapv (distance-point-line slope intercept) xs ys)]            
-            (- (* 1/2 k (square diffs))))))
-        
+  [xs ys k]
+  (fn [[_ [slope intercept] _]]
+    (let [diffs (mapv (distance-point-line slope intercept) xs ys)]
+      (- (* 1/2 k (square diffs))))))
 
-;; trying to draw _something_, prettify later
+
+;; trying to draw something,
+;; prettify later
+
+
 (defn draw-line [convert]
-(fn [{:keys [state color]}]
+  (fn [{:keys [state color]}]
     (q/background 100)
     (q/fill color 255 255)
     (let [[slope intercept] (convert state)]
-        (attach [0 intercept] [10 ((linear-func slope intercept) 10)])
-        (bob [0 intercept])
-        (bob [10 ((linear-func slope intercept) 10)])))))
- 
+      (attach [0 intercept] [10 ((linear-func slope intercept) 10)])
+      (bob [0 intercept])
+      (bob [10 ((linear-func slope intercept) 10)]))))
+
+
 ;; fake data        
+
+
 (def x (gen-xs 10))
 (def y (gen-ys x 2 3))
 
 ;; animate
-(let [L (L-regression x y 1)
-    initial-state (up 1
+(let [L (L-regression-composed x y 1)
+      initial-state (up 1
                         (up 1 1)
                         1)
-    built (build L initial-state)]
-(q/defsketch regression
+      built (build L initial-state)]
+  (q/defsketch regression
     :title "Regression line"
     :size [500 500]
     :setup (:setup built)
